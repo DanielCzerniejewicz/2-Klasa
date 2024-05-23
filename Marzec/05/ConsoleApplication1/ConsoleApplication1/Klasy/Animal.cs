13cb32fc7bd37638e8dfd8564a144548c9244aa1
Daniel Czerniejewicz <programowaniedaniel@gmail.com>
2024-05-23 10:08:09 +0200

    Add files via upload

---
+++ Marzec/05/ConsoleApplication1/ConsoleApplication1/Klasy/Animal.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ConsoleApplication1.Klasy
+{
+    enum Rodzaj
+    {
+        Ssak,
+        Gad,
+        Plaz,
+        Ptak,
+        Ryba
+    }
+    internal class Animal
+    {
+        /*
+         * Każde zwierzę powinno mieć następujące właściwości: nazwę, datę urodzenia,
+         * informację czy jest ssakiem i rodzaj (ptak, ryba, gad, płaz lub ssak).
+        Program powinien umożliwiać dodawanie nowych zwierząt do listy, podając ich właściwości od użytkownika.
+        Program powinien umożliwiać wyświetlanie listy zwierząt z ich numerami i nazwami.
+        Program powinien umożliwiać wyświetlanie szczegółów o wybranym zwierzęciu, takich jak opis, wiek i rodzaj.
+        Program powinien umożliwiać usuwanie wszystkich lub pojedynczego zwierzęcia z listy.
+        Program powinien mieć menu główne z opcjami do wyboru przez użytkownika.
+        Program powinien być napisany zgodnie z konwencją nazewnictwa i formatowania kodu w C#.
+         */
+        public string Imie { get; set; }
+        public bool CzySsak { get; set; }
+        public DateTime Data_Urodzenia { get; set; }
+        public Rodzaj Rodzaj { get; set; }
+
+        public Animal(string imie, DateTime dataUrodzenia, bool czySsak, Rodzaj kind) : this(imie, dataUrodzenia,
+            czySsak)
+        {
+            Rodzaj = kind;
+        }
+        public Animal(string imie, DateTime dataUrodzenia, bool czySsak): this(imie,dataUrodzenia)
+        {
+            CzySsak = czySsak;
+        }
+        public Animal(string imie, DateTime dataUrodzenia) : this(imie)
+        {
+            Data_Urodzenia = dataUrodzenia;
+        }
+        public Animal(string imie)
+        {
+            Imie = imie;
+            CzySsak = true;
+            Data_Urodzenia = DateTime.Now;
+            Rodzaj = Rodzaj.Ssak;
+        }
+
+        //metoda opisujaca zwierzaka
+        public string GetData()
+        {
+            string description =
+                $"To jest {Imie}! Data Urodzenia : {Data_Urodzenia.ToShortDateString()} ";
+            if (CzySsak == true)
+            {
+                description += "Zwierze jest ssakiem";
+            }
+            else
+            {
+                description += $"Zwierzę nie jest ssakiem jest : {Rodzaj}";
+            }
+            return description;
+        }
+
+        public void ShowAge()
+        {
+            Console.WriteLine($"{Imie} ma {DateTime.Now.Year - Data_Urodzenia.Year} lat");
+        }
+        
+    }
+}
---
+++ Marzec/05/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleApplication1.Klasy;
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Animal a = new Animal("Burek");
+            Console.WriteLine(a.GetData());
+            a.Data_Urodzenia = new DateTime(2014, 2, 3);
+            a.ShowAge();
+            //Tworzenie Listy zwierząt
+            List<Animal> zwierzaki = new List<Animal>();
+        }
+        
+        static void ShowMainMenu(List<Animal> animals)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Witaj w programie do zarządzania zwierzętami");
+            Console.WriteLine("Wybierz jedną z opcji : ");
+            Console.WriteLine("1. Dodaj zwierzę");
+            Console.WriteLine("2. Pokaż Listę zwierząt");
+            Console.WriteLine("3. Pokaż listę zwierząt");
+            Console.WriteLine("4. Usuń zwierze");
+            Console.WriteLine("5. Zakończ Program");
+            string wybor = Console.ReadLine();
+            switch (wybor)
+            {
+                case "1":
+                    
+                    break;
+            }
+        }
+    }
+    
+}
